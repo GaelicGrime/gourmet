05269b3328c88f10aef0dd3f2f5ff9abe18f4bfd
GaelicGrime <will.angus.blaylock@gmail.com>
2024-02-08 10:02:34 +0000

    20240208_030234

--- src/gourmet/GourmetRecipeManager.py
+++ src/gourmet/GourmetRecipeManager.py
@@ -964,8 +964,6 @@ class RecGui (RecIndex, GourmetApplication, ImporterExporter, StuffThatShouldBeP
         self.conf.append(saver)
 
         # <<>>
-        print(f"""dbargs.custom_url \"{dbargs['custom_url']}\"<<>>"""")
-        # <<>>
         self.window.set_default_size(*self.prefs['app_window']['window_size'])
         self.window.set_title(version.appname)
         self.main = Gtk.VBox()
