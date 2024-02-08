a6ebbcdb682de9293108d8cafa3f6f68dcde0cd3
GaelicGrime <will.angus.blaylock@gmail.com>
2024-02-08 11:21:18 +0000

    20240208_042118

--- src/gourmet/GourmetRecipeManager.py
+++ src/gourmet/GourmetRecipeManager.py
@@ -1,6 +1,6 @@
 
 
-import json
+import pprint
 
 import re
 import threading
@@ -976,7 +976,7 @@ class RecGui (RecIndex, GourmetApplication, ImporterExporter, StuffThatShouldBeP
         ##    _window_title_ = f"""{version.appname} Using DB \"{args.db_url}\"."""
         ##else:
         _window_title_ = version.appname
-        print(f"""{json.dumps(globals())}""")
+        print(f"""{pprint.pprint(globals())}""")
         # <<>>
         self.window.set_default_size(*self.prefs['app_window']['window_size'])
         self.window.set_title(_window_title_)
