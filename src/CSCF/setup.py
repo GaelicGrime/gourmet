1a3630f8b2bc438d288083ff7d3ebc5497930729
GaelicGrime <will.angus.blaylock@gmail.com>
2024-11-01 21:25:18 +0000

    20241101_152518

--- src/CSCF/setup.py
+++ src/CSCF/setup.py
@@ -2,12 +2,11 @@
 
 from setuptools import find_packages
 from setuptools import setup
-#from rsaaf import __VERSION__ as VS
 from datetime import datetime as DT
 
 
 __VERSION__ = DT.now().strftime("%Y.%m.%d.%H%M%S")
-with open("__PACKAGE_NAME__/__VERSION__.py", "tw") as _FD_OUT_:
+with open("CSCF/__VERSION__.py", "tw") as _FD_OUT_:
   _FD_OUT_.write(f"""\n\n__VERSION__ = \"{__VERSION__}\"\n\n#\n""")
 
 
@@ -15,22 +14,12 @@ setup(
   author="GaelicGrime",
   author_email="will.angus.blaylock@gmail.com",
   license="GPLv3",
-  name="__PACKAGE_NAME__",
-  url="https://github.com/ComfortableSoftware/__PACKAGE_NAME__",
+  name="CSCF",
+  url="https://github.com/ComfortableSoftware/CSCF",
   version=__VERSION__,
-  package_dir={"__PACKAGE_NAME__": "__PACKAGE_NAME__"},
-  package_data={
-      "__PACKAGE_NAME__": [
-          "../doc/*",
-      ],
-  },
+  package_dir={"CSCF": "CSCF"},
   packages=find_packages(),
-  install_requires=[
-      "CSCF",
-  ],
-  scripts=[
-      "scripts/__PACKAGE_NAME__",
-  ],
+  install_requires=[],
 )
 
 
