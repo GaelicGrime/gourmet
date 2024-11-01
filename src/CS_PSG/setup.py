94e5067a10570aa0982f9a552b7f3c606197dfb8
GaelicGrime <will.angus.blaylock@gmail.com>
2024-11-01 22:40:22 +0000

    20241101_164022

--- src/CS_PSG/setup.py
+++ src/CS_PSG/setup.py
@@ -2,12 +2,11 @@
 
 from setuptools import find_packages
 from setuptools import setup
-#from rsaaf import __VERSION__ as VS
 from datetime import datetime as DT
 
 
 __VERSION__ = DT.now().strftime("%Y.%m.%d.%H%M%S")
-with open("__PACKAGE_NAME__/__VERSION__.py", "tw") as _FD_OUT_:
+with open("CS_PSG/__VERSION__.py", "tw") as _FD_OUT_:
   _FD_OUT_.write(f"""\n\n__VERSION__ = \"{__VERSION__}\"\n\n#\n""")
 
 
@@ -15,21 +14,18 @@ setup(
   author="GaelicGrime",
   author_email="will.angus.blaylock@gmail.com",
   license="GPLv3",
-  name="__PACKAGE_NAME__",
-  url="https://github.com/ComfortableSoftware/__PACKAGE_NAME__",
+  name="CS_PSG",
+  url="https://github.com/ComfortableSoftware/CS_PSG",
   version=__VERSION__,
-  package_dir={"__PACKAGE_NAME__": "__PACKAGE_NAME__"},
+  package_dir={"CS_PSG": "CS_PSG"},
   package_data={
-      "__PACKAGE_NAME__": [
+      "CS_PSG": [
           "../doc/*",
       ],
   },
   packages=find_packages(),
   install_requires=[
-      "CSCF",
-  ],
-  scripts=[
-      "scripts/__PACKAGE_NAME__",
+      "PySimpleGUI",
   ],
 )
 
